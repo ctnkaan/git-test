9a99d431c6960d42ed1f6583e235fd3449ff8376
ctnkaan <kaan.taskingenc@gmail.com>
2023-12-26 20:32:32 +0300

    app.js eklendi

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 console.log("Hello world!")
 console.log("Hello world!")
 console.log("Hello world!")
+console.log("Hello woorld!!!")
 console.log("Hello world!")
-console.log("Hello world!")
-console.log("Hello world!")
-console.log("Hello world!")
-console.log("Hello world!")
+
+console.log("test")
